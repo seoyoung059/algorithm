801f493ff690953c1e6cd937d2136528fea3ec28
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2023-04-23 21:14:34 +0900

    [Gold III] Title: 아침 산책, Time: 456 ms, Memory: 64780 KB -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -2,31 +2,37 @@ import sys
 sys.setrecursionlimit(10**6)
 
 n = int(sys.stdin.readline())
-a = input()
+a = sys.stdin.readline().rstrip()
 
 g=[[] for i in range(n+1)]
 for i in range(1,n):
-    k,l = map(int,sys.stdin.readline().split())
+    k,l = [int(x) for x in sys.stdin.readline().split()]
     g[k].append(l)
     g[l].append(k)
     
-def dfs(g,s,a,visited):
-    visited[s]=True
+def dfs(g,s,a):
+    global flag
+    # visited[s]=True
+    flag=flag | 1 << s
     cnt=0
     for i in g[s]:
-        if not visited[i]:
+        # if not visited[i]:
+        if flag & 1 << i:
             if a[i-1]=='0':
                 cnt+=dfs(g,i,a,visited)
             else:
                 cnt+=1
+    # print(cnt)
     return cnt
 
 def sol(n,g,a):
+    global flag
     cnt=0
     for i in range(1,n+1):
         if a[i-1]=='1':
-            visited=[False]*(n+1)
-            cnt+=dfs(g,i,a,visited)
+            # visited=[False]*(n+1)
+            cnt+=dfs(g,i,a)
     return cnt
 
+flag=0
 print(sol(n,g,a))
