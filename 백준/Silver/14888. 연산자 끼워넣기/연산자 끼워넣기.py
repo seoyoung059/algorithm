c693075b15ae601481858b6f991f9d56fdb78a74
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2023-04-24 10:33:08 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,49 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-def calculation(i,ans,next):
-    if i==0:
-        return ans+next
-    elif i==1:
-        return ans-next
-    elif i==2:
-        return ans*next
-    elif i==3:
-        if ans < 0:
-            return -((-ans)//next)
-        else:
-            return ans//next
-        
-def dfs(a,i,c,cal_num,ans):
-    global maxval
-    global minval
-    cal_num[c]-=1
-    ans=calculation(c,ans,a[i+1])
-        
-    
-    if cal_num==[0,0,0,0]:
-        if maxval < ans:
-            maxval=ans
-        if minval>ans:
-            minval=ans
-    
-    for next_c in range(4):
-        cal = cal_num.copy()
-        if cal[next_c]>0:
-            dfs(a,i+1,next_c,cal,ans)
-            
-def sol(a,cal_num):
-    for c in range(4):
-        cal=cal_num.copy()
-        if cal[c]>0:
-            dfs(a,0,c,cal,a[0])
-    print(maxval)
-    print(minval)
-    
-n=int(input())                          # 수열 개수
-a=list(map(int,input().split()))        # 수열
-cal = list(map(int,input().split()))    # +, -, *, /
+def sol1(ans,i,add,sub,mul,div):
+    global maxval,minval
 
-maxval=float("-inf")
-minval=10**10+1
-sol(a,cal)
+    if i==n:
+        maxval=max(maxval,ans)
+        minval=min(minval,ans)
+        return
+    if add:
+        sol1(ans+nums[i],i+1,add-1,sub,mul,div)
+    if sub:
+        sol1(ans-nums[i],i+1,add,sub-1,mul,div)
+    if mul:
+        sol1(ans*nums[i],i+1,add,sub,mul-1,div)
+    if div:
+        sol1(int(ans/nums[i]),i+1,add,sub,mul,div-1)
+
+n=int(input())                          # 수열 개수
+nums=list(map(int,input().split()))        # 수열
+a,s,m,d = map(int,input().split())
+
+maxval=-10**10
+minval=10**10
+sol1(nums[0],1,a,s,m,d)
+
+print(maxval)
+print(minval)
