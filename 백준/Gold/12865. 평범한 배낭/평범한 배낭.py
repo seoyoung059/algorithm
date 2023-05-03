c563722baf5e042bbf6d5f039060a847e5e3eb85
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2023-05-04 08:44:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 3256 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -7,7 +7,7 @@ for _ in range(n):
     things.append(list(map(int,input().split())))
     
 bag=[0]*(k+1)
-things.sort()
+
 for w,v in things:
     for i in range(k,w-1,-1):
         bag[i]=max(bag[i],bag[i-w]+v)
