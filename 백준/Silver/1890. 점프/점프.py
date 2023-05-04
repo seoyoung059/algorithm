28b2d7b74f746126bb57dd93dfa4537de6971d38
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2023-05-04 11:49:38 +0900

    [Silver I] Title: 점프, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.py
+++ 백준/Silver/1890. 점프/점프.py
@@ -1,31 +1,20 @@
 import sys
 input=sys.stdin.readline
 
-def findway(cy,cx):
+n = int(input())
 
-    if board[cy][cx]==0:
-       return 0
-    if arr[cy][cx]!=0:
-       return arr[cy][cx]
-    if arr[cy][cx]==0:
-       for i in range(2):
-           ny = cy+dy[i]*board[cy][cx]
-           nx = cx+dx[i]*board[cy][cx]
-           if 0<=ny<n and 0<=nx<n:
-               if ny==n-1 and nx==n-1:
-                   arr[cy][cx]=1
-               elif arr[ny][nx]!=0:
-                   arr[cy][cx]+=arr[ny][nx]
-               else:
-                   arr[cy][cx]+=findway(ny,nx)
-       return arr[cy][cx]
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-n=int(input())
-board = [list(map(int,input().split())) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
+dp[0][0] = 1
 
-
-dy = [1,0]
-dx = [0,1]
-arr = [[0] * n for _ in range(n)]
-findway(0,0)
-print(arr[0][0])
+for i in range(n):
+    for j in range(n):
+        if i == n-1 and j== n-1:
+            print(dp[i][j])
+            break
+        current = arr[i][j]
+        if current + j < n:
+            dp[i][j+current] += dp[i][j]
+        if current + i < n:
+            dp[i+current][j] += dp[i][j]
