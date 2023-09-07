e11af940d8f13032fc4e98cfaed9e31b39d9da22
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2023-09-07 13:18:32 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    for c in commands:
+        tmp = array[c[0]-1:c[1]]
+        tmp.sort()
+        answer.append(tmp[c[2]-1])
+        # print(tmp)
+    return answer
