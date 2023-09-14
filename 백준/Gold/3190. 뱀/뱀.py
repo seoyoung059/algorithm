283ae9557e3b07ab26e2ca7a70458804753e3a52
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2023-09-14 10:16:54 +0900

    [Gold IV] Title: 뱀, Time: 164 ms, Memory: 116428 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,63 +1,54 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
 
 n = int(input())
 k = int(input())
 
-apple=[]
-move=deque()
-for _ in range(k):
-    a = list(map(int,input().split()))
-    apple.append(a)
+apples = [[0]*n for _ in range(n)]
+for i in range(k):
+    a, b = map(int, input().split())
+    apples[a-1][b-1] = 1
+    
 l = int(input())
-for _ in range(l):
-    sec, turn = map(str,input().split())
-    move.append([int(sec),turn])
+turns = []
+for i in range(l):
+    t, dir = input().split()
+    turns.append([int(t), dir])
     
-sec = 0
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-current_head = [1,1]
-current=deque([current_head])
-d = 1
-while True:
-    # print(current)
-    sec += 1
+collision = False
+# 0 - 오른쪽, 1 - 아래쪽, 2 - 왼쪽, 3 - 아래쪽
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
 
-    # 몸 길이를 늘려 머리를 다음 칸에 위치시킴
-    current_head = [current_head[0]+dy[d],current_head[1]+dx[d]]
-    if current_head in current:
-        print(sec)
+start_head =[0,0]
+curr_dir = 0
+
+snake = deque()
+snake.append(start_head)
+cnt = 0
+idx = 0
+
+while not collision:
+    curr_head = snake[-1]
+    
+    #방향 전환
+    if idx < l:
+        if cnt  == turns[idx][0]:
+            if turns[idx][1] == 'D':
+                curr_dir = (curr_dir+1)%4
+            elif turns[idx][1] == 'L':
+                curr_dir = (curr_dir-1)%4
+            idx += 1
+    # 머리 다음칸 이동
+    next_head = [curr_head[0]+dy[curr_dir], curr_head[1]+dx[curr_dir]]
+    # 벽/몸일시 게임 끝
+    if not (0<= next_head[0]<n and 0<=next_head[1]<n) or next_head in snake:
         break
-    current.append(current_head)
+    # 사과 있으면 꼬리 안 움직임
+    if apples[next_head[0]][next_head[1]]==0:
+        snake.popleft()
+    else:
+        apples[next_head[0]][next_head[1]]=0
 
-    #이동한 칸에 사과가 있다면, 칸의 사과가 사라지고 꼬리는 움직이지 않음
-    eat_apple = False
-    for a in range(len(apple)):
-        if current_head == apple[a]:
-           eat_apple = True
-          #  apple[a]=[-1,-1]
-           apple.pop(a)
-           break
-    
-    if not eat_apple:
-        current.popleft()
-
-    
-    if not (0<current_head[0]<=n) or not (0<current_head[1]<=n):
-        print(sec)
-        break
-    
-    if len(move)>0:
-      if move[0][0]==sec:
-          # print('turn', move[0][1])
-          if move[0][1]=='L':
-              d += 1
-              if d >=4:
-                  d = d%4
-          else:
-              d -= 1
-              if d < 0:
-                  d += 4
-          move.popleft()
+    snake.append(next_head)
+    cnt+=1
+print(cnt+1)
