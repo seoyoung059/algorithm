090db53caca5911887c097627627dd95d70f4aef
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2023-10-06 23:08:47 +0900

    [Gold V] Title: 경쟁적 전염, Time: 204 ms, Memory: 119516 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,38 +1,34 @@
-import sys
+# 경쟁적 전염
 from collections import deque
-input = sys.stdin.readline
-
-def bfs(graph,virus,s,x,y):
-    queue=deque(virus)
-    while queue:
-        # for i in graph:
-        #     print(i)
-        v_num,cy,cx,cnt=queue.popleft()
-        # print(cnt,s)
-        if cnt==s:
-            print(graph[x-1][y-1])
-            return
-        for i in range(4):
-            ny=cy+dy[i]
-            nx=cx+dx[i]
-            if 0<=ny<n and 0<=nx<n and graph[ny][nx]==0:
-                graph[ny][nx]=v_num
-                queue.append((v_num,ny,nx,cnt+1))
-    print(graph[x-1][y-1])
-    
-
 n,k = map(int, input().split())
-graph=[]
-virus=[]
-for i in range(n):
-    row=list(map(int,input().split()))
-    graph.append(row)
-    for j in range(n):
-        if row[j]!=0:
-            virus.append((row[j],i,j,0))
-s,x,y = map(int,input().split())
-virus.sort()
+arr = []
+virus = []
 
-dy = [0,0,1,-1]
-dx = [1,-1,0,0]
-bfs(graph,virus,s,x,y)
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    for j,t in enumerate(tmp):
+        if t!=0:
+            virus.append([t, i, j, 0])
+    arr.append(tmp)
+
+s,x,y = map(int, input().split())
+
+virus.sort()
+q=deque(virus)
+
+dy = [1, -1, 0, 0]
+dx = [0, 0, 1, -1]
+
+while q:
+    v_num, cy, cx, sec = q.popleft()
+
+    if sec >= s:
+        break
+    for i in range(4):
+        ny = cy+dy[i]
+        nx = cx+dx[i]
+        if 0<=ny<n and 0<=nx<n and arr[ny][nx]==0:
+            arr[ny][nx]=v_num
+            q.append([v_num, ny, nx, sec+1])
+
+print(arr[x-1][y-1])
