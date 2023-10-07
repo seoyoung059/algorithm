fd5fbcada22d8c7dec392592a40993dc32e2a588
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2023-10-07 19:03:26 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 252 ms, Memory: 118260 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,29 +1,37 @@
-import sys
-input = sys.stdin.readline
+# 연산자 끼워 넣기
 
-def sol1(ans,i,add,sub,mul,div):
-    global maxval,minval
+n = int(input())
+arr = list(map(int, input().split()))
+a, b, c, d = map(int, input().split())
 
-    if i==n:
-        maxval=max(maxval,ans)
-        minval=min(minval,ans)
-        return
-    if add:
-        sol1(ans+nums[i],i+1,add-1,sub,mul,div)
-    if sub:
-        sol1(ans-nums[i],i+1,add,sub-1,mul,div)
-    if mul:
-        sol1(ans*nums[i],i+1,add,sub,mul-1,div)
-    if div:
-        sol1(int(ans/nums[i]),i+1,add,sub,mul,div-1)
+def sol(ans, i, a, b, c, d):
+    if i == n:
+        return [ans, ans]
 
-n=int(input())                          # 수열 개수
-nums=list(map(int,input().split()))        # 수열
-a,s,m,d = map(int,input().split())
+    minval = +1000000000
+    maxval = -1000000000    
+    if a:
+        maxtmp, mintmp = sol(ans+arr[i], i+1, a-1, b, c, d)
+        minval = min(minval, mintmp)
+        maxval = max(maxval, maxtmp)
+    if b:
+        maxtmp, mintmp = sol(ans-arr[i], i+1, a, b-1, c, d)
+        minval = min(minval, mintmp)
+        maxval = max(maxval, maxtmp)
+    if c:
+        maxtmp, mintmp = sol(ans*arr[i], i+1, a, b, c-1, d)
+        minval = min(minval, mintmp)
+        maxval = max(maxval, maxtmp)
+    if d:
+        if ans < 0:
+            maxtmp, mintmp = sol(-(-ans//arr[i]), i+1, a, b, c, d-1)        
+        else:
+            maxtmp, mintmp = sol(ans//arr[i], i+1, a, b, c, d-1)
+        minval = min(minval, mintmp)
+        maxval = max(maxval, maxtmp)
+    return [maxval, minval]
 
-maxval=-10**10
-minval=10**10
-sol1(nums[0],1,a,s,m,d)
 
-print(maxval)
-print(minval)
+ans = sol(arr[0], 1, a,b,c,d)
+for a in ans:
+    print(a)
