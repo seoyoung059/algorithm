eaf7f62e21bc35998665d2349f3b5f0fd6dd30ac
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2023-10-10 20:44:39 +0900

    [Silver II] Title: 병사 배치하기, Time: 136 ms, Memory: 110728 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -0,0 +1,16 @@
+# 병사 배치하기
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+dp = [1 for i in range(n)]
+
+ans = 1
+for i in range(1,n):
+    for j in range(i):
+        if arr[j] > arr[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+            ans = max(ans, dp[i])
+
+# print(dp)
+print(n-ans)
