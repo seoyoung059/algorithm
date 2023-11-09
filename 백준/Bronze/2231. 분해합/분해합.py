902c264b36d04711de327405bb007662a22bddf7
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2023-11-09 20:41:37 +0900

    [Bronze II] Title: 분해합, Time: 1100 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,11 +1,14 @@
+def solve(n):
+    ans = 0
+    for i in range(n):
+        tmp = str(i)
+        val = i
+        for t in tmp:
+            val+=int(t)
+        if val == n:
+            ans = i
+            return ans
+    return ans
+
 n = int(input())
-ans = 0
-for i in range(n):
-    num = str(i)
-    sum = i
-    for c in num:
-        sum+=int(c)
-    if sum == n:
-        ans = i
-        break
-print(ans)
+print(solve(n))
