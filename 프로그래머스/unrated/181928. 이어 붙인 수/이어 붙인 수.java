4320b07620cbe97c57ce80555a6351c5cba8d8b1
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2023-11-09 22:17:43 +0900

    [level unrated] Title: 이어 붙인 수, Time: 11.55 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String even = "";
+        String odd = "";
+        
+        for(int i=0;i<num_list.length;i++){
+            if(num_list[i]%2==0){
+                even+=num_list[i];
+            }
+            else{
+                odd+=num_list[i];
+            }
+        }
+        answer = 0+Integer.parseInt(even)+Integer.parseInt(odd);
+        return answer;
+    }
+}
