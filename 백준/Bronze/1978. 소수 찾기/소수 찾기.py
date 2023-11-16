4833f4011bfe6cae451aa2b02cc1b599a8347143
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2023-11-16 22:26:18 +0900

    [Bronze II] Title: 소수 찾기, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,14 +1,16 @@
 n = int(input())
-c = list(map(int, input().split()))
-count=0
-for i in c:
-    k=True
+arr = list(map(int, input().split()))
+
+cnt = 0
+
+for i in arr:
+    tmp = True
     if i==1:
-        k=False
-    for j in range(2,i):
-        if i % j==0:
-            k=False
+        tmp = False
+    for j in range(2, i):
+        if i % j ==0:
+            tmp = False
             break
-    if k==True:
-        count+=1
-print(count)
+    if tmp:
+        cnt += 1
+print(cnt)
