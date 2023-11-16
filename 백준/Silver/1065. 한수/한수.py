28ca55e676db18f2d71007887395d28f5224ba4c
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2023-11-16 23:07:50 +0900

    [Silver IV] Title: 한수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,18 +1,15 @@
-
-n = int(input())
-
-cnt = 0
-
-if n<100:
-    print(n)
-else:
-    cnt = 99
-    for i in range(100, n+1):
-        tmp = str(i)
-        for s in range(len(tmp)-2):
-            if int(tmp[s])-int(tmp[s+1]) != int(tmp[s+1])-int(tmp[s+2]):
-                break
-            if s == len(tmp)-3:
-                cnt+=1
-
-    print(cnt)
+n=int(input())
+count=0
+for i in range(1,n+1):
+    if i < 100:
+        count+=1
+    else:
+        answer=True
+        str_i=str(i)
+        gap=int(str_i[1])-int(str_i[0])
+        for j in range(len(str_i)-1):
+            if int(str_i[j+1])-int(str_i[j])!=gap:
+                answer=False
+        if answer:
+            count+=1
+print(count)
