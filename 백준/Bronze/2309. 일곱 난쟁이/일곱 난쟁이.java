b7db147ada9ccc34f93eab6af046bba5cb27b371
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-01-22 22:34:24 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,34 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> dwarfs = new ArrayList<Integer>();
+
+        int[] arr = new int[9];
         int sum = 0;
-        int tmp;
         for (int i = 0; i < 9; i++) {
-            tmp = Integer.parseInt(br.readLine());
-            dwarfs.add(tmp);
-            sum+=tmp;
+            arr[i] = Integer.parseInt(br.readLine());
+            sum+=arr[i];
         }
-        dwarfs.sort(Integer::compareTo);
-        int[] dArr = dwarfs.stream().mapToInt(Integer::intValue).toArray();
+
+        Arrays.sort(arr);
 
         for (int i = 0; i < 8; i++) {
             for (int j = i+1; j < 9; j++) {
-                if (sum-dArr[i]-dArr[j]==100){
+                if (sum-arr[i]-arr[j]==100){
                     for (int k = 0; k < 9; k++) {
                         if (k!=i && k!=j)
-                            System.out.println(dArr[k]);
+                            System.out.println(arr[k]);
                     }
                     return;
                 }
             }
         }
-
-
     }
 }
