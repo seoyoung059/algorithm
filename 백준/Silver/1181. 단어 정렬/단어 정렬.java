f54d2117598792bea1167102de605314ee588a43
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-01-22 21:50:00 +0900

    [Silver V] Title: 단어 정렬, Time: 684 ms, Memory: 21808 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        TreeSet<String> ts = new TreeSet<String>(new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                if (o1.length()-o2.length()==0){
+                    return o1.compareTo(o2);
+                }
+                return o1.length() - o2.length();
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            ts.add(br.readLine());
+        }
+
+        for(String s:ts){
+            System.out.println(s);
+        }
+    }
+}
