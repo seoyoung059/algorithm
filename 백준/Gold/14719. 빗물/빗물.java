15df08c5cb395fabeb562104ced402796ced414f
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-01-23 20:01:18 +0900

    [Gold V] Title: 빗물, Time: 84 ms, Memory: 11660 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -7,10 +7,6 @@ import java.util.StringTokenizer;
 public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    private static int sumUp(int[] a, int[] b, int[] arr){
-        return Math.min(a[0],b[0])*(b[1]-a[1]-1)-(arr[b[1]-1]-arr[a[1]]);
-    }
-
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int h = Integer.parseInt(st.nextToken());
