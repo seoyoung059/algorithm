0336802aa8e5b5cbf56f29a84a9144a9942ec014
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-01-28 21:59:08 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 84 ms, Memory: 11816 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2 * n -i- 1; j++) {
+                if (i <= j)
+                    sb.append("*");
+                else sb.append(" ");
+            }
+            sb.append("\n");
+        }
+        for (int i = n-2; i >= 0; i--) {
+            for (int j = 0; j < 2*n-1-i; j++) {
+                if (i<=j)
+                    sb.append("*");
+                else sb.append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
