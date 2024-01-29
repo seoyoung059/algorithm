df56518f251282300ed863a9e56041e918bcffd5
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-01-29 23:11:32 +0900

    [Gold IV] Title: 트리의 지름, Time: 368 ms, Memory: 26388 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -10,40 +10,33 @@ public class Main {
     static StringTokenizer st;
     static TreeNode[] arr;
     static TreeNode root;
-//    static PriorityQueue<Integer> pq;
+    static LinkedList[] childArray;
     static int answer;
     static class TreeNode {
         int num;
         int parent;
         int weight;
-        int maxWeightSum;
-        LinkedList<TreeNode> childs;
 
 
         public TreeNode(int num, int parent, int weight){
             this.num = num;
             this.parent = parent;
             this.weight = weight;
-            this.childs = new LinkedList<>();
         }
 
     }
 
-    static int dfs(TreeNode currNode) {
-//        System.out.println("Node "+currNode.num);
-        if (currNode.childs.isEmpty())
-            return currNode.weight;
+    static int dfs(int currNode) {
+        if (childArray[currNode].isEmpty())
+            return arr[currNode].weight;
         PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2)->o2-o1);
-        for(TreeNode n: currNode.childs){
-            pq.offer(dfs(n));
+        for(Object n: childArray[currNode]){
+            pq.offer(dfs((int) n));
         }
-//        System.out.println(pq.toString());
         int max = pq.poll();
         int next = (pq.isEmpty())?0:pq.peek();
         answer = Math.max(answer,max+next);
-//        System.out.println("Node "+currNode.num + " "+answer);
-//        System.out.println(max+currNode.weight);
-        return max + currNode.weight;
+        return max + arr[currNode].weight;
 
     }
     public static void main(String[] args) throws IOException {
@@ -51,6 +44,10 @@ public class Main {
         arr = new TreeNode[n+1];
         root = new TreeNode(1,0, 0);
         arr[1] = root;
+        childArray = new LinkedList[n+1];
+        for (int i = 1; i < n+1; i++) {
+            childArray[i] = new LinkedList<Integer>();
+        }
         int parent; int curr; int weight;
 
         for (int i = 2; i < n+1; i++) {
@@ -60,14 +57,11 @@ public class Main {
             weight = Integer.parseInt(st.nextToken());
 
             arr[curr] = new TreeNode(curr,parent, weight);
-        }
-
-        for (int i = 2; i < n+1; i++) {
-            arr[arr[i].parent].childs.offer(arr[i]);
+            childArray[parent].offer(curr);
         }
 
         answer = 0;
-        dfs(root);
+        dfs(1);
 
         System.out.println(answer);
     }
