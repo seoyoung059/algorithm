22746cee438bf79927fc50206322d82ce59cc540
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-02-02 22:25:54 +0900

    [Gold IV] Title: 오큰수, Time: 988 ms, Memory: 210252 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,35 +1,47 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    static class Point {
+        int val;
+        int idx;
+        public Point (int val, int idx) {
+            this.val = val;
+            this.idx = idx;
+        }
+
+    }
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-        int[][] arr = new int[2][n];
+        int[] arr = new int[n];
+        Stack<Point> stk = new Stack<>();
+        int curr; Point tmp;
         for (int i = 0; i < n; i++) {
-            arr[0][i] = Integer.parseInt(st.nextToken());
+            curr = Integer.parseInt(st.nextToken());
+            while(!stk.isEmpty()){
+                if (stk.peek().val < curr){
+                    tmp = stk.pop();
+                    arr[tmp.idx] = curr;
+                }
+                else break;
+            }
+            stk.push(new Point(curr,i));
+        }
+        while(!stk.isEmpty()){
+            tmp = stk.pop();
+            arr[tmp.idx]=-1;
         }
 
-        Stack<Integer> stk = new Stack<>();
-        for(int i=n-1; i>=0; i--){
-            while(!stk.isEmpty()){
-                if (stk.peek() > arr[0][i]) break;
-                stk.pop();
-            }
-            if (!stk.isEmpty())
-                arr[1][i] = stk.peek();
-            else arr[1][i]=-1;
-            stk.push(arr[0][i]);
-        }
-//
-//        System.out.println(Arrays.toString(arr[1]));
-        for(int i: arr[1])
+        for (int i: arr){
             sb.append(i).append(" ");
+        }
         System.out.println(sb.toString());
     }
 }
