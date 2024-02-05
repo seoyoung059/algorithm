ecc0ebd831877acaafdffbb931dfc1ee2311a4c6
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-02-05 21:38:13 +0900

    [Gold V] Title: 탑, Time: 612 ms, Memory: 105964 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,43 +1,37 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+//import java.util.Stack;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-    static class Tower {
-        int h;
-        int idx;
-        public Tower(int h, int idx) {
-            this.h = h;
-            this.idx = idx;
-        }
-    }
-    static Stack<Tower> stk = new Stack<>();
-
-    static void solution(int n) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        int height; Tower t;
-        stk.push(new Tower(100_000_001, 0));
-        for (int i = 0; i < n; i++) {
-            height = Integer.parseInt(st.nextToken());
-            while(stk.peek().h < height){
-                stk.pop();
-            }
-            sb.append(stk.peek().idx);
-            sb.append(" ");
-            stk.push(new Tower(height, i+1));
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-
-        solution(n);
-        System.out.println(sb.toString());
-    }
+	
+	static class Tower {
+		int height;
+		int idx;
+		public Tower(int height, int idx) {
+			this.height = height;
+			this.idx = idx;
+		}
+	}
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		ArrayDeque<Tower> stk = new ArrayDeque<Tower>();
+		stk.push(new Tower(Integer.MAX_VALUE, 0));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int currHeight;
+		for(int i=1; i<=n; i++) {
+			currHeight = Integer.parseInt(st.nextToken());
+			while(currHeight > stk.peek().height) {
+				stk.pop();
+			}
+			sb.append(stk.peek().idx).append(" ");
+			stk.push(new Tower(currHeight, i));
+		}
+		
+		System.out.println(sb);
+	}
 }
