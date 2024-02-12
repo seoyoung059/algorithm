5896fb1fe28531da2fb01d325697b91115e19dcb
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-02-12 21:34:00 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 836 ms, Memory: 127184 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,28 +1,42 @@
-
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static BufferedReader br;
+    static int n;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[2000001];
-
-        for (int i = 0; i < n; i++) {
-            arr[Integer.parseInt(br.readLine())+1000000] = 1;
-        }
-
-        for (int i = 0; i < 2000001; i++) {
-            if (arr[i]==1){
-                sb.append(i-1000000);
-                sb.append("\n");
+    static void shellSort(int[] arr) {
+        int h = n/2;
+        int j, tmp;
+        while(h>0) {
+            for (int i = h; i < n; i++) {
+                j = i-h;
+                tmp = arr[i];
+                while(j>=0 && arr[j]>tmp){
+                    arr[j+h] = arr[j];
+                    j-=h;
+                }
+                arr[j+h] = tmp;
             }
+            h /= 2;
         }
-        System.out.println(sb.toString());
     }
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        
+        shellSort(arr);
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+        System.out.print(sb);
+    }
+
 }
