0a6044f62bc5b19302f18d7904f87da4981946f4
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-02-13 20:19:05 +0900

    [Silver IV] Title: 설탕 배달, Time: 76 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,23 +1,29 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
+
 public class Main {
-    private static int solution(int n) {
-        int a = n/5;
-        int b = n%5;
-        while (a > -1){
-            if (b%3==0)
-                return a+b/3;
-            else {
-                a--;
-                b += 5;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n+1];
+        
+        for(int i=1; i<n+1; i++){
+            if(i==3||i==5) arr[i] = 1;
+            else if(i-5>=0 && arr[i-5]>0 && arr[i-3]>0){
+                arr[i] = Math.min(arr[i-5], arr[i-3])+1;
             }
+            else if(i-5>=0&&arr[i-5]>0){
+                arr[i] = arr[i-5]+1;
+            }
+            else if(i-3>=0 && arr[i-3]>0){
+                arr[i] = arr[i-3]+1;
+            }
+            else arr[i]=-1;
         }
-        return -1;
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        System.out.println(solution(n));
-    }
+        
+        System.out.print(arr[n]);
+	}
+
 }
