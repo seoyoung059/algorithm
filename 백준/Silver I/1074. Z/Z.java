8737989220e2263512825544ead8f69ddf8cf5ba
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-02-15 19:08:03 +0900

    [Silver I] Title: Z, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Silver I/1074. Z/Z.java
+++ 백준/Silver I/1074. Z/Z.java
@@ -4,15 +4,12 @@ import java.util.StringTokenizer;
 
 public class Main {
 	
-	static int solution(int size, int r, int c) {
-		if(size==1) return r*2+c;
-		return 
-				(int)Math.pow(2*2, size-1)*
-				((r/((int)Math.pow(2, size-1))*2
-						+(c)/(int)Math.pow(2, size-1)))
-				+ solution(size-1, 
-						(int)(r%((int)(Math.pow(2, size-1)))),
-						(int)(c%((int)(Math.pow(2, size-1)))));
+	static int sol(int size, int r, int c) {
+		if(size==1) return 2*r+c;
+		int tmp = (int)Math.pow(2,  size-1);
+		
+		return (2*(r/tmp)+c/tmp)*tmp*tmp
+				+ sol(size-1, r%tmp, c%tmp);
 	}
 
 	public static void main(String[] args) throws Exception{
@@ -22,7 +19,7 @@ public class Main {
 		int r = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
 
-		System.out.println(solution(n,r,c));
+		System.out.println(sol(n,r,c));
 		
 	}
 
