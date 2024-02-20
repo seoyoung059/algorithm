33bb7904ab69a7decaa8af6d20ed3bbe053999f2
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-02-20 21:03:39 +0900

    [Gold V] Title: 용액, Time: 304 ms, Memory: 32120 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int s, e, m;
+        int val = Integer.MAX_VALUE;
+        int ans1 = 0;
+        int ans2 = 0;
+        for (int i = 0; i < n-1; i++) {
+            s = i + 1;
+            e = n-1;
+            while (s < e) {
+                m = (s + e) / 2;
+                if (arr[i] + arr[m] < 0) {
+                    s = m + 1;
+                } else e = m;
+            }
+            int abs = Math.abs(arr[i] + arr[e]);
+            if (abs < val) {
+                val = abs;
+                ans1 = arr[i];
+                ans2 = arr[e];
+            }
+            abs = Math.abs(arr[i] + arr[e - 1]);
+            if (i < e - 1 && abs < val) {
+                val = abs;
+                ans1 = arr[i];
+                ans2 = arr[e - 1];
+            }
+        }
+        System.out.println(ans1 + " " + ans2);
+    }
+}
