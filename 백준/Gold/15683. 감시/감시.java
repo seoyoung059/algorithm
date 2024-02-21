de58b0244fd3027e09fc3588420ce396dc55caf2
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-02-21 22:27:04 +0900

    [Gold IV] Title: 감시, Time: 196 ms, Memory: 54636 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -19,107 +19,104 @@ public class Main {
         }
         return newArr;
     }
-    static int sol(int idx, int[][] visited) {
+    static int sol(int idx, int[][] visited, int cnt) {
+//        System.out.println(cnt);
         if(idx == cNum){
-            int ans = 0;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    ans+=(visited[i][j]==0)?1:0;
-                }
-            }
-            return ans;
+            return cnt;
         }
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
         int y = cctv[idx][0];
         int x = cctv[idx][1];
         int[] dy = {1, 0, -1, 0};
         int[] dx = {0, 1, 0, -1};
 
         int[][] newVisited;
-        int ny, nx, d;
+        if(visited[y][x]==0) cnt++;
+        visited[y][x] = 1;
+        int ny, nx, d, tmp;
         switch (arr[y][x]){
             case 1:
                 for (int dir = 0; dir < 4; dir++) {
-                    d = dir;
+                    d = dir; tmp = 0;
                     newVisited = deepcopy(visited);
-                    newVisited[y][x] = 1;
                     ny = y + dy[d];
                     nx = x + dx[d];
                     while(isValid(ny, nx) && arr[ny][nx]!=6){
+                        if(newVisited[ny][nx]==0) tmp++;
                         newVisited[ny][nx]=1;
                         ny+=dy[d];
                         nx+=dx[d];
                     }
-                    answer = Math.min(answer,sol(idx+1, newVisited));
+                    answer = Math.max(answer,sol(idx+1, newVisited, cnt+tmp));
                 }
                 break;
             case 2:
                 for (int dir = 0; dir < 2; dir++) {
-                    d = dir;
+                    d = dir; tmp = 0;
                     newVisited = deepcopy(visited);
-                    newVisited[y][x] =1;
                     for (int i = 0; i < 2; i++) {
                         ny = y+dy[d];
                         nx = x+dx[d];
                         while(isValid(ny, nx) && arr[ny][nx]!=6 ){
+                            if(newVisited[ny][nx]==0) tmp++;
                             newVisited[ny][nx]=1;
                             ny+=dy[d];
                             nx+=dx[d];
                         }
                         d = (d+2)%4;
                     }
-                    answer = Math.min(answer,sol(idx+1, newVisited));
+                    answer = Math.max(answer,sol(idx+1, newVisited, cnt+tmp));
                 }
                 break;
             case 3:
                 for (int dir = 0; dir < 4; dir++) {
-                    d = dir;
+                    d = dir; tmp = 0;
                     newVisited = deepcopy(visited);
-                    newVisited[y][x] = 1;
                     for (int i = 0; i < 2; i++) {
                         ny = y+dy[d];
                         nx = x+dx[d];
                         while(isValid(ny, nx) && arr[ny][nx]!=6 ){
+                            if(newVisited[ny][nx]==0) tmp++;
                             newVisited[ny][nx]=1;
                             ny+=dy[d];
                             nx+=dx[d];
                         }
                         d = (d+1)%4;
                     }
-                    answer = Math.min(answer,sol(idx+1, newVisited));
+                    answer = Math.max(answer,sol(idx+1, newVisited, cnt+tmp));
                 }
                 break;
             case 4:
                 for (int dir = 0; dir < 4; dir++) {
-                    d = dir;
+                    d = dir; tmp = 0;
                     newVisited = deepcopy(visited);
-                    newVisited[y][x] =1;
                     for (int i = 0; i < 3; i++) {
                         ny = y+dy[d];
                         nx = x+dx[d];
                         while(isValid(ny, nx) && arr[ny][nx]!=6 ){
+                            if(newVisited[ny][nx]==0) tmp++;
                             newVisited[ny][nx]=1;
                             ny+=dy[d];
                             nx+=dx[d];
                         }
                         d = (d+1)%4;
                     }
-                    answer = Math.min(answer,sol(idx+1, newVisited));
+                    answer = Math.max(answer,sol(idx+1, newVisited, cnt+tmp));
                 }
                 break;
             case 5:
-                newVisited = visited;
-                newVisited[y][x] = 1;
+                newVisited = visited; tmp = 0;
                 for (int i = 0; i < 4; i++) {
                     ny = y+dy[i];
                     nx = x+dx[i];
                     while(isValid(ny, nx) && arr[ny][nx]!=6 ){
+                        if(newVisited[ny][nx]==0) tmp++;
                         newVisited[ny][nx]=1;
                         ny+=dy[i];
                         nx+=dx[i];
                     }
                 }
-                answer = Math.min(answer,sol(idx+1, newVisited));
+                answer = Math.max(answer,sol(idx+1, newVisited, cnt+tmp));
                 break;
         }
         return answer;
@@ -149,7 +146,7 @@ public class Main {
         }
 
         int[][] visited = new int[n][m];
-        System.out.println(sol(0, visited)-wall);
+        System.out.println(n*m-sol(0, visited, 0)-wall);
 
     }
 }
