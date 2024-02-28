6f217452cb2996e3c5d92096d8f939d89953cbf0
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-02-28 20:26:18 +0900

    [Silver II] Title: 이동하기, Time: 432 ms, Memory: 80888 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -3,31 +3,43 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int n, m;
+	static int[][] dp;
+	static BufferedReader br;
+	static StringTokenizer st;
+	
+	static int sol(int[][] arr, int y, int x) {
+		if(!(0<=y && y<n && 0<=x &&x<m)) return 0;
+		if(dp[y][x]!=-1) return dp[y][x];
+		dp[y][x]  =  Math.max(sol(arr, y-1, x), Math.max(sol(arr, y, x-1), sol(arr, y-1, x-1)))+arr[y][x];
+		return dp[y][x];
+	}
+	
+	static void sol2() throws Exception{
+		int[][] arr = new int[n][m];
+		dp = new int[n][m];
+
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<m; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				dp[i][j] = -1;
+			}
+		}
+		
+		System.out.print(sol(arr, n-1, m-1));
+		
+	}
 
 	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m  = Integer.parseInt(st.nextToken());
 		
-		int n = Integer.parseInt(st.nextToken());
-		int m  = Integer.parseInt(st.nextToken());
-		
-		int[][][] arr = new int[n+1][m+1][2];
 
-		
-		for(int i=1; i<n+1; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<m+1; j++) {
-				arr[i][j][0] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		for(int i=1; i<n+1; i++) {
-			for(int j=1; j<m+1; j++) {
-				arr[i][j][1] = Math.max(arr[i-1][j][1], Math.max(arr[i][j-1][1], arr[i-1][j-1][1]))+arr[i][j][0];
-			}
-		}
-		
-		System.out.println(arr[n][m][1]);
+		sol2();
 	}
 
 }
