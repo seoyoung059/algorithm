8783e03b3db6f3d026a075881c5ff552459f9dc8
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-03-06 23:36:47 +0900

    [Gold V] Title: 내일 할거야, Time: 1912 ms, Memory: 284580 KB -BaekjoonHub

---
+++ 백준/Gold/7983. 내일 할거야/내일 할거야.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/* 09:10*/
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o->-o[1]));
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            pq.offer(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+        }
+
+        int now = Integer.MAX_VALUE;
+        int[] curr;
+        while(!pq.isEmpty()){
+            curr = pq.poll();
+            now = Math.min(now, curr[1])-curr[0];
+        }
+        System.out.println(now);
+    }
+}
