3d89ed51cd26bebbbf2311b3b7c00674c6dc6864
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-06-07 18:09:44 +0900

    [Gold IV] Title: 카우버거 알바생, Time: 124 ms, Memory: 12436 KB -BaekjoonHub

--- 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.java
+++ 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.java
@@ -1,31 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static class Order implements Comparable{
+    static class Order {
         int x;
         int y;
 
-        Order (int x, int y) {
+        Order(int x, int y) {
             this.x = x;
             this.y = y;
-        }
-
-        @Override
-        public int compareTo(Object o) {
-            Order tmp = (Order) o;
-//            if(this.x == tmp.x) return this.y - tmp.y;
-            return this.x - tmp.x;
-        }
-
-        @Override
-        public String toString(){
-            return "["+x+" "+y+"]";
         }
     }
 
@@ -37,17 +23,17 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        Order[] arr = new Order[n+1];
+        Order[] arr = new Order[n + 1];
         arr[0] = new Order(-1, -1);
-        for (int i = 1; i < n+1; i++) {
+        for (int i = 1; i < n + 1; i++) {
             st = new StringTokenizer(br.readLine());
             arr[i] = new Order(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
 
-        int[][] dp = new int[m+1][k+1];
+        int[][] dp = new int[m + 1][k + 1];
         int answer = 0;
         int x, y;
-        for (int i = 1; i < n+1; i++) {
+        for (int i = 1; i < n + 1; i++) {
             x = arr[i].x;
             y = arr[i].y;
             for (int j = m; j >= x; j--) {
