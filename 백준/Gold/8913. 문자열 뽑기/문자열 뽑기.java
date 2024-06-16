9bea0be45845bb352c15cef635170e64a4b054c6
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-06-16 13:14:41 +0900

    [Gold III] Title: 문자열 뽑기, Time: 1032 ms, Memory: 131532 KB -BaekjoonHub

---
+++ 백준/Gold/8913. 문자열 뽑기/문자열 뽑기.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    static HashMap<String, Boolean> hm = new HashMap<>();
+
+    static boolean solve(String str) {
+        int cnt; String nextStr; boolean answer = false;
+        switch (str.length()){
+            case 0:
+                return true;
+            case 1:
+                return false;
+            default:
+                if(hm.get(str)!=null) return hm.get(str);
+                cnt = 1;
+                for (int i = 1; i < str.length(); i++) {
+                    if(str.charAt(i-1)== str.charAt(i)){
+                        cnt++;
+                    } else {
+                        if(cnt>=2){
+                            nextStr = str.substring(0, i-cnt).concat(str.substring(i, str.length()));
+                            hm.put(nextStr, solve(nextStr));
+                            answer |= hm.get(nextStr);
+                        }
+                        cnt = 1;
+                    }
+                }
+                if(cnt>=2) {
+                    nextStr = str.substring(0, str.length()-cnt);
+                    hm.put(nextStr, solve(nextStr));
+                    answer|=hm.get(nextStr);
+                }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        String str;
+        for (int i = 0; i < n; i++) {
+            str = br.readLine();
+            if(solve(str)){
+                sb.append(1);
+            } else sb.append(0);
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
