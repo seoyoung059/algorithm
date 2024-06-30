728fd7f1f2423640de4db6e1f8edc9096f0a738b
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-06-30 23:18:04 +0900

    [Gold III] Title: 아침 산책, Time: 6980 ms, Memory: 229496 KB, Score: 108 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**4)
 
 n = int(sys.stdin.readline())
 a = sys.stdin.readline().rstrip()
@@ -13,24 +13,25 @@ for i in range(1,n):
 def dfs(g,s,a):
     global flag
     # visited[s]=True
-    flag=flag | 1 << s
+    flag = flag|1<<s
     cnt=0
     for i in g[s]:
-        # if not visited[i]:
-        if flag & 1 << i:
+        if flag & 1<<i:
+            pass
+        else:
             if a[i-1]=='0':
-                cnt+=dfs(g,i,a,visited)
+                cnt+=dfs(g,i,a)
             else:
                 cnt+=1
-    # print(cnt)
     return cnt
 
 def sol(n,g,a):
-    global flag
     cnt=0
+    global flag
     for i in range(1,n+1):
         if a[i-1]=='1':
             # visited=[False]*(n+1)
+            flag=0
             cnt+=dfs(g,i,a)
     return cnt
 
