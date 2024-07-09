41c3a2568afaa103df3a366a5b577408c6a7ed84
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-07-10 00:25:54 +0900

    [Gold I] Title: ABC, Time: 120 ms, Memory: 17532 KB -BaekjoonHub

--- 백준/Gold/12969. ABC/ABC.java
+++ 백준/Gold/12969. ABC/ABC.java
@@ -2,10 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static void makeString(){
+
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,91 +21,62 @@ public class Main {
 
         ArrayDeque<int[]> q = new ArrayDeque<>();
         q.offer(new int[] {0, 0, 0});
-        int[] curr = null; int b=0, c=0, kk=0, qSize;
-        for (int i = 0; i < n; i++)  {
+        int[] curr; int b, c, kk = 0, qSize;
+        loop: for (int i = 0; i < n; i++)  {
             qSize = q.size();
             while(qSize-- > 0) {
                 curr = q.poll();
-//                System.out.println(Arrays.toString(curr));
                 if (curr[0] + curr[1] + curr[2] <= k && dp[curr[0]][curr[1]][curr[0] + curr[1] + curr[2]] == 0) {
                     dp[curr[0]][curr[1]][curr[0] + curr[1] + curr[2]] = 1;
                     q.offer(new int[]{curr[0], curr[1], curr[0] + curr[1] + curr[2]});
-                    if (q.peekLast()[2] == k) {
-                        b = q.peekLast()[0];
-                        c = q.peekLast()[1];
-                        kk = q.peekLast()[2];
-//                        System.out.println(dp[b][c][kk]);
-                        break;
-                    }
+                    if (q.peekLast()[2] == k) break loop;
                 }
 
                 if (curr[0] < n && curr[1] + curr[2] <= k && dp[curr[0] + 1][curr[1]][curr[1] + curr[2]] == 0) {
                     dp[curr[0] + 1][curr[1]][curr[1] + curr[2]] = 2;
                     q.offer(new int[]{curr[0] + 1, curr[1], curr[1] + curr[2]});
-                    if (q.peekLast()[2] == k) {
-                        b = q.peekLast()[0];
-                        c = q.peekLast()[1];
-                        kk = q.peekLast()[2];
-//                        System.out.println(dp[b][c][kk]);
-                        break;
-                    }
+                    if (q.peekLast()[2] == k) break loop;
                 }
 
                 if (curr[1] < n && dp[curr[0]][curr[1] + 1][curr[2]] == 0) {
                     dp[curr[0]][curr[1] + 1][curr[2]] = 3;
                     q.offer(new int[]{curr[0], curr[1] + 1, curr[2]});
-                    if (q.peekLast()[2] == k) {
-                        b = q.peekLast()[0];
-                        c = q.peekLast()[1];
-                        kk = q.peekLast()[2];
-//                        System.out.println(dp[b][c][kk]);
-                        break;
-                    }
+                    if (q.peekLast()[2] == k) break loop;
                 }
             }
         }
 
         int tmp;
-        if(kk!=k){
+        if(q.peekLast()[2]!=k){
             System.out.println(-1);
         } else {
-            ArrayDeque<Integer> ans = new ArrayDeque<>();
+            StringBuilder sb = new StringBuilder();
+            b = q.peekLast()[0];
+            c = q.peekLast()[1];
+            kk = q.peekLast()[2];
             tmp = dp[b][c][kk];
             while(b!=0 || c!=0 || kk!=0){
-//                System.out.println(b+" "+c+" "+kk+" "+tmp);
-                ans.offerLast(tmp);
                 switch (tmp){
                     case 1:
+                        sb.append('A');
                         kk -= b+c;
                         break;
                     case 2:
+                        sb.append('B');
                         kk -= c;
                         b -= 1;
                         break;
                     case 3:
+                        sb.append('C');
                         c -= 1;
                         break;
                 }
                 tmp = dp[b][c][kk];
             }
-
-            StringBuilder sb = new StringBuilder();
-//            sb.repeat('C',n-ans.size());
-            for (int i = 0; i < n-ans.size(); i++) {
-                sb.append('C');
-            }
-            for(Integer i: ans){
-                switch (i){
-                    case 1:
-                        sb.append('A');
-                        break;
-                    case 2:
-                        sb.append('B');
-                        break;
-                    case 3:
-                        sb.append('C');
-                        break;
-                }
+            
+            tmp = n-sb.length();
+            for (int i = 0; i < tmp; i++) {
+                sb.append('A');
             }
             System.out.println(sb);
         }
