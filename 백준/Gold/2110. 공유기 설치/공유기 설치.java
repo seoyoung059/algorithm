ded03af6978e432e2f5be37c2e8eb77ea2e119d6
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-08-22 22:10:54 +0900

    [Gold IV] Title: 공유기 설치, Time: 220 ms, Memory: 28500 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -5,25 +5,26 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
 
-    static int[] count(int m) {
-        int cnt = 1; int prev = arr[0];
-        int tmp = Integer.MAX_VALUE;
+    static int[] arr;
+    static int n;
+
+    static int count(int minVal) {
+        int cnt = 1, prev = arr[0];
         for (int i = 1; i < arr.length; i++) {
-            if(arr[i]-prev >= m) {
+            if (arr[i] - prev >= minVal) {
                 cnt++;
-                tmp = Math.min(tmp, arr[i]-prev);
                 prev = arr[i];
             }
         }
-        return new int[] {cnt, tmp};
+        return cnt;
     }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
 
         arr = new int[n];
@@ -32,19 +33,15 @@ public class Main {
         }
         Arrays.sort(arr);
 
-        int s = 1, e = arr[n-1]-arr[0], m = (s+e)/2;
-        int[] tmp; int answer = 0;
-        while(s <= e){
-            m = (s+e)/2;
-            tmp = count(m);
-            if(tmp[0] >= c){
-                s = m+1;
-                answer = Math.max(answer, tmp[1]);
-            }
-            else {
-                e = m-1;
-            }
+        int start = 0, end = arr[n-1]-arr[0]+1, mid;
+        int tmp;
+        while(start < end) {
+            mid = (start + end)/2;
+
+            if(count(mid) < c) end = mid;
+            else start = mid + 1;
         }
-        System.out.println(answer);
+
+        System.out.println(start - 1);
     }
 }
