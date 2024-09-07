4a15b43209dfd99d70bddab2314d9f0e38767cb4
seoyoung059 <58359639+seoyoung059@users.noreply.github.com>
2024-09-07 13:35:03 +0900

    [Gold III] Title: ACM Craft, Time: 736 ms, Memory: 242536 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -1,66 +1,66 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int solve() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+1];
+        int[] cnt = new int[n+1];
+        int[] time = new int[n+1];
+        ArrayDeque[] deques = new ArrayDeque[n+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < n+1; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            deques[i] = new ArrayDeque<Integer>();
+        }
+
+        int a, b;
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            cnt[b]++;
+            deques[a].offerLast(b);
+        }
+        int w = Integer.parseInt(br.readLine());
+
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        for (int i = 1; i < n+1; i++) {
+            if(cnt[i]==0) q.offerLast(i);
+        }
+
+        int curr; int next;
+        while(!q.isEmpty()) {
+            curr = q.pollFirst();
+            time[curr] += arr[curr];
+            if(curr == w) break;
+            while(!deques[curr].isEmpty()) {
+                next = (int) deques[curr].pollFirst();
+                cnt[next]--;
+                time[next] = Math.max(time[next], time[curr]);
+                if(cnt[next]==0) {
+                    q.offerLast(next);
+                }
+            }
+        }
+
+        return time[w];
+    }
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+
         int T = Integer.parseInt(br.readLine());
 
-        int n, k, a, b, w;
-        int[] d, indegree, answer;
-        ArrayDeque[] order;
-        ArrayDeque<Integer> zeroIndegree = new ArrayDeque<>();
-        for (int tc = 1; tc <= T; tc++) {
-            zeroIndegree.clear();
-            st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            k = Integer.parseInt(st.nextToken());
-
-            order = new ArrayDeque[n+1];
-            d = new int[n+1];
-            indegree = new int[n+1];
-            answer = new int[n+1];
-            st = new StringTokenizer(br.readLine());
-            for (int i = 1; i < n+1; i++) {
-                d[i] = Integer.parseInt(st.nextToken());
-                order[i] = new ArrayDeque<Integer>();
-            }
-
-
-            for (int i = 0; i < k; i++) {
-                st = new StringTokenizer(br.readLine());
-                a = Integer.parseInt(st.nextToken());
-                b = Integer.parseInt(st.nextToken());
-                indegree[b]++;
-                order[a].offer(b);
-            }
-            w = Integer.parseInt(br.readLine());
-
-            for (int i = 1; i < n+1; i++) {
-                if(indegree[i]==0) {
-                    zeroIndegree.offer(i);
-                }
-            }
-
-            while(!zeroIndegree.isEmpty()){
-                a = zeroIndegree.poll();
-                answer[a] += d[a];
-                if(a==w) break;
-                while(!order[a].isEmpty()){
-                    b = (int) order[a].poll();
-                    indegree[b]--;
-                    answer[b] = Math.max(answer[b], answer[a]);
-                    if(indegree[b]==0)
-                        zeroIndegree.offer(b);
-                }
-            }
-            sb.append(answer[w]).append("\n");
+        for (int tc = 0; tc < T; tc++) {
+            System.out.println(solve());
         }
-        System.out.println(sb);
     }
+
 }
